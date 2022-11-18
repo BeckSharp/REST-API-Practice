a56e7316c585dc964c9f2896fbeec5ad1cf134a3
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-11-18 16:57:45 +0000

    Database model created

---
+++ Model/Database.php
@@ -0,0 +1,36 @@
+<?php
+class Database {
+    protected $connection = null;
+
+    public function __construct() {
+        try {
+            $this->connection = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DATABASE_NAME);
+
+            if (mysqli_connect_errno()) {
+                throw new Exception("Could not connect to database.");
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    public function select($query = "", $parameters = []) {
+        try {
+            $statement = $this->connection->prepare($query);
+
+            if($statement === false) {
+                throw new Exception("Unable to do prepared statement: " . $query);
+            }
+
+            if ($params) {
+                $statement->binde_param($parameters[0], $parameters[1]);
+            }
+
+            $statement->execute();
+
+            return $statement;
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
+    }
+}
---
+++ inc/config.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_HOST", "secret");
+define("DB_USERNAME", "secret");
+define("DB_PASSWORD", "secret");
+define("DATABASE_NAME", "secret");
+?>
